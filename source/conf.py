42fb659d9229e0d34228b30f216a44cd4bfa139c
Christian Perfect <christianperfect@gmail.com>
2018-06-12 09:44:19 +0100

    bump to version 3

--- source/conf.py
+++ source/conf.py
@@ -19,8 +19,8 @@ master_doc = 'index'
 project = u'Numbas'
 copyright = u'2012-2018, Newcastle University'
 
-version = '2.0'
-release = '2.0'
+version = '3.0'
+release = '3.0'
 
 import sphinx_rtd_theme
 html_theme = 'sphinx_rtd_theme'
